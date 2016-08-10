ca5c662d7959b93ccb16ddc07987059732c73073
lilingang <lilingang@smartdevices.com.cn>
2016-08-10 18:37:18 +0800

    * 修正宏定义

--- DDLogger/DDLogger.h
+++ DDLogger/DDLogger.h
@@ -11,10 +11,10 @@
 #import "DDLoggerClient.h"
 #import "DDLoggerManager.h"
 
-#define DDLogError(args...)           DDExtendNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,DDLogLevelError,args);
-#define DDLogWarning(args...)         DDExtendNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,DDLogLevelWarning,args);
-#define DDLog(args...)                DDExtendNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,DDLogLevelInfo,args);
-#define DDLogDebug(args...)           DDExtendNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,DDLogLevelDebug,args);
+#define DDLogError(...)           DDExtendNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,DDLogLevelError,[NSString stringWithFormat:__VA_ARGS__]);
+#define DDLogWarning(...)         DDExtendNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,DDLogLevelWarning,[NSString stringWithFormat:__VA_ARGS__]);
+#define DDLog(...)                DDExtendNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,DDLogLevelInfo,[NSString stringWithFormat:__VA_ARGS__]);
+#define DDLogDebug(...)           DDExtendNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,DDLogLevelDebug,[NSString stringWithFormat:__VA_ARGS__]);
 
 
 #endif /* DDLogger_h */
